4f71b47689f2fc643bd157151c3cc4004f18645f
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2024-12-15 09:49:28 +0200

    last version

--- TODO.asm
+++ TODO.asm
@@ -3,32 +3,51 @@
 
 .DATA
     menu_msg        DB 13,10,'=== To-Do List Manager ===',13,10
-    DB '1. Add Task',13,10
-    DB '2. View Tasks',13,10
-    DB '3. Exit',13,10
-    DB 'Choose option (1-3): $'
+                    DB '1. Add Task',13,10
+                    DB '2. View Tasks',13,10
+                    DB '3. Display Task Count',13,10
+                    DB '4. Exit',13,10
+                    DB 'Choose option (1-4): $'
     
     input_msg       DB 13,10,'Enter task: $'
     tasks_header    DB 13,10,'=== Your Tasks ===',13,10,'$'
     no_tasks_msg    DB 13,10,'No tasks available.',13,10,'$'
     task_prefix     DB 13,10,'- $'
+    task_count_msg  DB 13,10,'Current Number of Tasks: $'
     max_tasks       equ 10
     max_task_len    equ 50
     
-    tasks           DB max_tasks * max_task_len dup('$')  ; Array to store tasks
-    task_count      DB 0                                  ; Counter for number of tasks
-    buffer          DB max_task_len                       ; Buffer for reading input
-    DB ?                                  ; Actual chars read
-    DB max_task_len dup('$')             ; Input buffer
+    tasks           DB max_tasks * max_task_len dup('$')
+    task_count      DB 0
+    buffer          DB max_task_len
+                    DB ?
+                    DB max_task_len dup('$')
 
-    .CODE
-    main PROC FAR
+.CODE
+; Procedure to display the number of tasks
+display_task_count PROC NEAR
+    ; Display task count message
+    MOV AH, 09H
+    LEA DX, task_count_msg
+    INT 21H
+
+    ; Convert task count to ASCII for display
+    MOV AL, task_count
+    ADD AL, 30H   ; Convert number to ASCII digit
+    MOV DL, AL     ; Move digit to DL for display
+    MOV AH, 02H    ; DOS interrupt for character output
+    INT 21H
+
+    RET            ; Return to caller
+    display_task_count ENDP
+
+main PROC FAR
     .STARTUP
     
 menu:
     ; Display menu
     MOV AH, 09H
-    LEA dx, menu_msg
+    LEA DX, menu_msg
     INT 21H
     
     ; Get user choice
@@ -41,7 +60,9 @@ menu:
     CMP AL, '2'
     JE view_tasks
     CMP AL, '3'
-    JMP exit_program
+    JE show_task_count
+    CMP AL, '4'
+    JE exit_program
     JMP menu
 
 add_task:
@@ -70,7 +91,7 @@ add_task:
     
     ; Copy input to tasks array
     LEA BX, buffer + 2
-    MOV CH, buffer + 1
+    MOV CL, buffer + 1
     XOR CH, CH
 copy_loop:
     MOV AL, [BX]
@@ -115,13 +136,17 @@ display_loop:
     ADD SI, max_task_len
     
     POP CX
-    LOOP display_loop
+    loop display_loop
     JMP menu
 
 no_tasks:
     MOV AH, 09H
     LEA DX, no_tasks_msg
     INT 21H
+    JMP menu
+
+show_task_count:
+    CALL display_task_count
     JMP menu
 
 exit_program:
